b8944836b4e508f1d32929454fee71d70793ae39
GastonEE01 <142426074+GastonEE01@users.noreply.github.com>
2026-07-18 00:16:10 -0300

    fix: elimino import sin usar

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css'
-import {  Route, Routes,Navigate } from 'react-router-dom'
+import {  Route, Routes } from 'react-router-dom'
 
 // Componentes
 import { Header } from './components/Header/Header'
