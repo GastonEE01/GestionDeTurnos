051a5cf98948bc4575cabd29d3c1e4d002f70b99
GastonEE01 <142426074+GastonEE01@users.noreply.github.com>
2026-07-18 00:15:09 -0300

    agrego ruta por defecto

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css'
-import {  Route, Routes } from 'react-router-dom'
+import {  Route, Routes,Navigate } from 'react-router-dom'
 
 // Componentes
 import { Header } from './components/Header/Header'
@@ -12,13 +12,20 @@ function App() {
     <>
      <div>
       <Routes>
-        <Route path='/Home' element={ 
+        <Route path='/Home' element={     
         <>
         <Header/>
         <Home/>
-        
-        </>}></Route>
-      </Routes>
+        </>}>
+        </Route>
+
+<Route path='/' element={     
+        <>
+        <Header/>
+        <Home/>
+        </>}>
+        </Route>
+       </Routes>
      </div>
     </>
   )
